b96fcc091fe66d1b1036632253e689aff829cff5
nik251021 <nikgvozdaryov@gmail.com>
2025-06-27 22:35:15 +0200

    First try

---
+++ build/CMakeFiles/MySDL2App.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MySDL2App.dir/main.cpp.obj"
+  "CMakeFiles/MySDL2App.dir/main.cpp.obj.d"
+  "MySDL2App.exe"
+  "MySDL2App.exe.manifest"
+  "MySDL2App.pdb"
+  "libMySDL2App.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MySDL2App.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/MySDL2App.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MySDL2App.
+# This may be replaced when dependencies are built.
